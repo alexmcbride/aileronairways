ca82cc1be5c674f02e9b04a2d0c4458704b4237e
Alex McBride <xandermcbride@gmail.com>
2018-02-18 11:33:08 +0000

    Added some doc comments to TestUtils

--- TimelineApi.Tests/TestUtils.cs
+++ TimelineApi.Tests/TestUtils.cs
@@ -6,13 +6,19 @@ namespace Echelon.TimelineApi.Tests
 {
     public static class TestUtils
     {
-        // Verifies that a particular property is present in an object.
-        public static bool VerifyObject(object obj, string key, object value)
+        /// <summary>
+        /// Uses reflection to verify that a particular property appears in an object.
+        /// </summary>
+        /// <param name="obj">The object to test.</param>
+        /// <param name="name">The name of the property.</param>
+        /// <param name="value">The value of the property.</param>
+        /// <returns>True if it exists.</returns>
+        public static bool VerifyObject(object obj, string name, object value)
         {
             var properties = obj.GetType().GetProperties();
             foreach (var prop in properties)
             {
-                if (prop.Name == key)
+                if (prop.Name == name)
                 {
                     return prop.GetValue(obj) == value;
                 }
@@ -20,6 +26,12 @@ namespace Echelon.TimelineApi.Tests
             return false;
         }
 
+        /// <summary>
+        /// Gets a completed task with the specified result.
+        /// </summary>
+        /// <typeparam name="T">The type of the result.</typeparam>
+        /// <param name="result">The result.</param>
+        /// <returns>A completed task.</returns>
         public static Task<T> GetCompletedTask<T>(T result)
         {
             var tcs = new TaskCompletionSource<T>();
@@ -27,6 +39,12 @@ namespace Echelon.TimelineApi.Tests
             return tcs.Task;
         }
 
+        /// <summary>
+        /// Gets a task that throws a particular exception.
+        /// </summary>
+        /// <typeparam name="TResult">The result the task expects.</typeparam>
+        /// <param name="exception">The exception object to throw.</param>
+        /// <returns>The dodgy task that causes an error.</returns>
         public static Task<TResult> GetExceptionTask<TResult>(WebException exception)
         {
             var tcs = new TaskCompletionSource<TResult>();
