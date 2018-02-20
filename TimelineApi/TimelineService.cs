1ec53eb08d5f5f98f9bb4ac567022c0da1bd7ff4
Alex McBride <xandermcbride@gmail.com>
2018-02-20 20:44:50 +0000

    Removed CleanupResponse as IdeaGen have now fixed this issue.

--- TimelineApi/TimelineService.cs
+++ TimelineApi/TimelineService.cs
@@ -28,13 +28,6 @@ namespace Echelon.TimelineApi
             return $"{_baseUrl}{resource}";
         }
 
-        private static string CleanupResponse(string value)
-        {
-            // Need to remove these characters from response or JSON does not deserialize correctly. Not sure if this 
-            // is an issue with IdeaGen's API, or something on our end. Anyway, this works.
-            return value.Replace("\\", string.Empty).Trim('"');
-        }
-
         private void HandleError(WebException ex)
         {
             // Check if this was a 400 or 500 message.
@@ -58,8 +51,7 @@ namespace Echelon.TimelineApi
             {
                 // Make request and get JSON response.
                 string url = GetUrl(resource);
-                string response = await _client.UploadStringAsync(url, body.ToString());
-                return CleanupResponse(response);
+                return await _client.UploadStringAsync(url, body.ToString());
             }
             catch (WebException ex)
             {
@@ -84,8 +76,7 @@ namespace Echelon.TimelineApi
             {
                 // Get JSON response.
                 string url = GetUrl(resource);
-                string response = await _client.DownloadStringAsync(url, headers);
-                return CleanupResponse(response);
+                return await _client.DownloadStringAsync(url, headers);
             }
             catch (WebException ex)
             {
