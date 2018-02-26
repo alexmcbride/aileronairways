d2028bac78dd947357b75e7bff7e6640a88855c7
Alex McBride <alexmcbride@users.noreply.github.com>
2018-02-26 17:03:02 +0000

    Tweaks to attachments

--- AileronAirwaysWeb/Controllers/TimelineEventsController.cs
+++ AileronAirwaysWeb/Controllers/TimelineEventsController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -37,8 +36,6 @@ namespace AileronAirwaysWeb.Controllers
                 .Where(e => !e.IsDeleted)
                 .OrderByDescending(e => e.EventDateTime)
                 .ToList();
-
-
 
             return View(timelineEvents);
         }
--- TimelineApi.TestConsole/Program.cs
+++ TimelineApi.TestConsole/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Echelon.TimelineApi.TestConsole
 {
@@ -13,8 +12,8 @@ namespace Echelon.TimelineApi.TestConsole
         static void Main(string[] args)
         {
             //RunTestsAsync();
-            //RunAttachmentTestsAsync();
-            RunTimelineCollectionTestsAsync();
+            RunAttachmentTestsAsync();
+            //RunTimelineCollectionTestsAsync();
 
             // Stop program from exiting.
             Console.ReadKey(true);
@@ -47,13 +46,13 @@ namespace Echelon.TimelineApi.TestConsole
             DisplayTimelineEvent(evt);
 
             Console.WriteLine("Creating attachment");
-            Attachment attachment = await Attachment.CreateAsync(api, evt.Id, "Test3.txt");
+            Attachment attachment = await Attachment.CreateAsync(api, evt.Id, "Attachment Title");
             DisplayAttachment(attachment);
             Console.WriteLine("Done");
 
             Console.WriteLine("Uploading attachment");
-            File.Copy("Test3.txt", attachment.Id);
-            await attachment.UploadAsync(api, attachment.Id);
+            //File.Copy("Test3.txt", attachment.Id);
+            await attachment.UploadAsync(api, "Test3.txt");
             Console.WriteLine("Done");
 
             Console.WriteLine("Downloading attachment");
