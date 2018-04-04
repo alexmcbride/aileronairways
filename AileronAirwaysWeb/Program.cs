a1aa5214d3cee09ea688b944b4801f1fb36b5b9a
Alex McBride <xandermcbride@gmail.com>
2018-04-05 00:36:30 +0100

    Map location marker does not show in timeline events if no location set

--- AileronAirwaysWeb/Program.cs
+++ AileronAirwaysWeb/Program.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.IO;
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
 
 namespace AileronAirwaysWeb
 {
@@ -19,65 +14,5 @@ namespace AileronAirwaysWeb
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-        //MS doc kestred connection
-        //public static void Main(string[] args)
-        //{
-        //    BuildWebHost(args).Run();
-        //}
-        //public static IWebHost BuildWebHost(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //    .UseStartup<Startup>()
-        //    .UseKestrel(options =>
-        //    {
-        //        options.Listen(IPAddress.Loopback, 44346, listenOptions =>
-        //                {
-        //                    listenOptions.UseHttps("c:\\tmp\\localhost.pfx", "P55sw0rd");
-        //              });
-        //    })
-        //            .UseIISIntegration()
-        //            .UseContentRoot(Directory.GetCurrentDirectory())
-        //            .UseStartup<Startup>()
-        //            .UseUrls("https://localhost:44346/")
-        //            .Build();
-
-
-        //blog 
-        //public static void Main(string[] args)
-        //{
-        //    var config = new ConfigurationBuilder()
-        //        .SetBasePath(Directory.GetCurrentDirectory())
-        //        .AddEnvironmentVariables()
-        //        .AddJsonFile("certificate.json", optional: true, reloadOnChange: true)
-        //        .AddJsonFile($"certificate.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
-        //        .Build();
-
-        //    var certificateSettings = config.GetSection("certificateSettings");
-        //    string certificateFileName = certificateSettings.GetValue<string>("filename");
-        //    string certificatePassword = certificateSettings.GetValue<string>("password");
-
-        //    var certificate = new X509Certificate2(certificateFileName, certificatePassword);
-
-        //    var host = new WebHostBuilder()
-        //        .UseKestrel(
-        //            options =>
-        //            {
-        //                options.AddServerHeader = false;
-        //                options.Listen(IPAddress.Loopback, 44346, listenOptions =>
-        //                {
-        //                    listenOptions.UseHttps(certificate);
-        //                });
-        //            }
-        //        )
-        //        .UseIISIntegration()
-        //        .UseConfiguration(config)
-        //        .UseContentRoot(Directory.GetCurrentDirectory())
-        //        .UseStartup<Startup>()
-        //        .UseUrls("https://localhost:44346/")
-        //        .Build();
-
-        //    host.Run();
-        //}
-
     }
 }
