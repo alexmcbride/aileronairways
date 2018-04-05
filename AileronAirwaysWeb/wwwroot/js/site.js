d9a09f0091a6a7d646f92037ade356767bbd7270
Alex McBride <xandermcbride@gmail.com>
2018-04-05 01:00:25 +0100

    Added delete modal to timeline event details

--- AileronAirwaysWeb/wwwroot/js/site.js
+++ AileronAirwaysWeb/wwwroot/js/site.js
@@ -7,3 +7,47 @@ function disableFormElement(element, disabled) {
         $(element).removeAttr("disabled");
     }
 }
+
+function displayDeleteModal(data, id, timelineId) {
+    $('#myModalContent').html(data);
+    $('#myModal').modal({ keyboard: true });
+    $('#myModal').modal('show');
+
+    // Hook event to edit view form.
+    $('#delete-event-form').on('submit', function (e) {
+        e.preventDefault();
+
+        disableFormElement('#btnSave', true);
+
+        var data = $('#delete-event-form').serialize();
+        $.post('/timelines/@ViewBag.TimelineId/events/' + id + '/delete', data, function (response) {
+            if (response.startsWith('OK')) {
+                var eventId = response.split(' ')[1];
+                location.href = '/timelines/' + timelineId+'/events';
+                $('#myModal').modal('hide');
+            }
+            else {
+                displayDeleteModal(response, id);
+            }
+
+            disableFormElement('#btnSave', false);
+        }, 'html');
+    });
+}
+
+function handleDelete() {
+    var id = $('#deleteEvent').attr('data-id');
+    var timelineId = $('#deleteEvent').attr('data-timeline-id');
+    $.ajax({
+        type: "GET",
+        url: "/Timelines/" + timelineId + "/Events/" + id + "/Delete",
+        contentType: "application/json; charset=utf-8",
+        datatype: "json",
+        success: function (data) {
+            displayDeleteModal(data, id, timelineId);
+        },
+        error: function () {
+            alert("Dynamic content load failed.");
+        }
+    });
+}
