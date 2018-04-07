8be92e82b4d2d65fcccb9f7a3c035737b56a3b29
Alex McBride <xandermcbride@gmail.com>
2018-04-07 23:51:42 +0100

    Tweaks to edit description stuff

---
+++ AileronAirwaysWeb/Extensions/HtmlExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace AileronAirwaysWeb.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static IHtmlContent KeepLines(this IHtmlHelper helper, string content)
+        {
+            return helper.Raw(content.Replace(Environment.NewLine, "<br>"));
+        }
+    }
+}
