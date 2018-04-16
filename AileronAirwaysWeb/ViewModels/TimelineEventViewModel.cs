b7277a1dfb66117de6c88479e375897ea04de10b
Mie Tanaka <mietta25@gmail.com>
2018-04-16 22:07:22 +0100

    Merge branch 'dev-sprint-7' of https://github.com/alexmcbride/echelon into dev-sprint-7

--- AileronAirwaysWeb/ViewModels/TimelineEventViewModel.cs
+++ AileronAirwaysWeb/ViewModels/TimelineEventViewModel.cs
@@ -32,9 +32,9 @@ namespace AileronAirwaysWeb.ViewModels
     {
         [Required]    
         public string Location { get; set; }
-        [RegularExpression(@"^(\\+|-)?(?:85(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-4])(?:(?:\\.[0-9]{1,6})?))$", ErrorMessage = "No details available for this latitude input:valid input is more than -85 and less than 85, data you input does not save")]
+        [RegularExpression(@"^(\+|-)?(?:90(?:(?:\.0{1,15})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,15})?))$", ErrorMessage = "No details available for this latitude input:valid input is more than -85 and less than 85, data you input does not save")]
         public string latitude { get; set; }
-        [RegularExpression(@"^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$", ErrorMessage = "No details available for this longititude input: valid input is more than - 180 less than 180, data you input does not save")]
+        [RegularExpression(@"^(\\+|-)?(?:180(?:(?:\\.0{1,15})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,15})?))$", ErrorMessage = "No details available for this longititude input: valid input is more than - 180 less than 180, data you input does not save")]
         public string longitude { get; set; }
     }
 }
