cf074ee95e0da833746a55c4f238adf0669f6a00
Mie Tanaka <mietta25@gmail.com>
2018-04-21 10:14:53 +0100

    lat reg ex limit back to =-90 to 90

--- AileronAirwaysWeb/ViewModels/TimelineEventViewModel.cs
+++ AileronAirwaysWeb/ViewModels/TimelineEventViewModel.cs
@@ -34,7 +34,7 @@ namespace AileronAirwaysWeb.ViewModels
         public string Location { get; set; }
         [RegularExpression(@"^(\+|-)?(?:85(?:(?:\.0{1,50})?)|(?:[0-9]|[1-8][0-5])(?:(?:\.[0-9]{1,50})?))$", ErrorMessage = "No details available for this latitude input:valid input is more than -85 and less than 85, data you input does not save")]
         public string latitude { get; set; }
-        [RegularExpression(@"^(\\+|-)?(?:180(?:(?:\\.0{1,50})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,50})?))$", ErrorMessage = "No details available for this longititude input: valid input is more than - 180 less than 180, data you input does not save")]
+        [RegularExpression(@"^(\+|-)?(?:180(?:(?:\.0{1,50})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,50})?))$", ErrorMessage = "No details available for this longititude input: valid input is more than - 180 less than 180, data you input does not save")]
         public string longitude { get; set; }
     }
 }
